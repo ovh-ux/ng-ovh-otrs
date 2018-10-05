bd62dac10dcd212ab6deef2cb38d98867dbbd68d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-10-05 16:09:32 +0200

    chore(webpack): update dependencies and use webpack

--- src/otrs-popup/otrs-popup.provider.js
+++ src/otrs-popup/otrs-popup.provider.js
@@ -1,23 +1,18 @@
-angular.module("ovh-angular-otrs")
-    .provider("OtrsPopup", function () {
-        "use strict";
+export default function () {
+  const self = this;
+  let baseUrlTickets = null;
 
-        var self = this;
-        var baseUrlTickets = null;
+  self.setBaseUrlTickets = (url) => {
+    if (angular.isDefined(url) && angular.isString(url)) {
+      baseUrlTickets = url;
+    } else {
+      throw new Error('An URL must be specified.');
+    }
+  };
 
-        self.setBaseUrlTickets = function (url) {
-            if (angular.isDefined(url) && angular.isString(url)) {
-                baseUrlTickets = url;
-            } else {
-                throw new Error("An URL must be specified.");
-            }
-        };
-
-        self.$get = function () {
-            return {
-                getBaseUrlTickets: function () {
-                    return baseUrlTickets;
-                }
-            };
-        };
-    });
+  self.$get = () => ({
+    getBaseUrlTickets() {
+      return baseUrlTickets;
+    },
+  });
+}
